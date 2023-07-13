b34febc9eca78f0769f2145bc2ce9d05aa898cfe
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-07-13 11:08:31 +0200

    Update results.py

--- results.py
+++ results.py
@@ -1,6 +1,19 @@
+from types_definition import *
+
 class Results(object):
 
     def __init__(self, graph, particle_config, rotor_config):
+        """
+        Keep track of important informations during a routing:
+            - nb_steps: total number of steps for the routing
+            - nb_l_edges: number of edges going left (only relevant for simple path graph)
+            - nb_r_edges: number of edges going right (only relevant for simple path graph)
+            - edges_counter: a dictionnary, {edge: number of times taken}
+            - nodes_counter: a dictionnary, {node: number of times taken}
+            - nb_particles_in_sinks: a dictionnary, {sink: number of particles}
+            - last_visit: a dictionnary, {node: number of the step when it was last visited (between 0 and nb_steps)}
+            - configuration_history: the list of the configurations (rotor, particle) from oldest to newest
+        """
         self.nb_steps = 0
         self.nb_l_edges = 0
         self.nb_r_edges = 0
@@ -12,6 +25,9 @@ class Results(object):
         
 
     def __str__(self):
+        """
+        Gives structured representation of the results when called with print()
+        """
         res = f"Number of steps : {self.nb_steps}\n"
         if (self.nb_l_edges != 0) or (self.nb_r_edges != 0):
             res += f"Number of left edges : {self.nb_l_edges} \nNumber of right edges : {self.nb_r_edges}\n\n"
@@ -31,17 +47,25 @@ class Results(object):
 
         return res
 
-    def orientation_edges(self, rotor_config):
-
+    def orientation_edges(self, rotor_config: RotorConfig):
+        """
+        Count the number of left and right edges
+        Input:
+            - rotor_config: a RotorConfig from which to count
+        """
         for edge in rotor_config.values():
-            if edge[0] == (edge[1] + 1):
+            if edge[0] == (edge[1] + 1): # left
                 self.nb_l_edges += 1
-            if edge[0] == (edge[1] - 1):
+            if edge[0] == (edge[1] - 1): # right
                 self.nb_r_edges += 1
 
 
-    def particles_in_sinks(self, particle_config):
-
+    def particles_in_sinks(self, particle_config: ParticleConfig):
+        """
+        Update the number of particles in the sinks
+        Input:
+            - particle_config: a ParticleConfig from which to take the values
+        """
         for sink in self.nb_particles_in_sinks:
             self.nb_particles_in_sinks[sink] = particle_config[sink]
 
